181f10c8cd82dab17a41d510c04f4c4c968ade79
DiscoreMe <nikitabikmaev@gmail.com>
2016-10-14 15:46:52 +0300

    Add rest files

---
+++ MinecraftHelper/Forms/Tellraw.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MinecraftHelper.Forms
+{
+    public partial class TellrawForm : Form
+    {
+        private int width;
+        private int height;
+
+        public TellrawForm(int width, int height)
+        {
+            InitializeComponent();
+        }
+
+        private void Tellraw_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
